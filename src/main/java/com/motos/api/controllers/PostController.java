7485314cb9da9d3e9aef18bc5182c92fefec48e7
Pedro Escamilla <escamillalopezpedro0@gmail.com>
2026-05-26 18:53:29 -0600

    clean config

--- src/main/java/com/motos/api/controllers/PostController.java
+++ src/main/java/com/motos/api/controllers/PostController.java
@@ -1,10 +1,14 @@
 package com.motos.api.controllers;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
 import com.motos.api.models.Post;
 import com.motos.api.repositories.PostRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -16,32 +20,60 @@ public class PostController {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private Cloudinary cloudinary;
+
     @GetMapping
     public List<Post> getPosts() {
         return postRepository.findAll();
     }
 
     @PostMapping
-    public Post createPost(@RequestBody Post post) {
+    public Post createPost(
+
+            @RequestParam("text") String text,
+
+            @RequestParam("image")
+            MultipartFile image
+
+    ) throws Exception {
+
+        String imageUrl = cloudinary.uploader()
+                .upload(
+                        image.getBytes(),
+                        ObjectUtils.emptyMap()
+                )
+                .get("secure_url")
+                .toString();
+
+        Post post = new Post();
+
+        post.setText(text);
+        post.setImageUrl(imageUrl);
+
         return postRepository.save(post);
     }
- 
+
     @DeleteMapping("/{id}")
-    public void deletePost(@PathVariable Long id) {
+    public void deletePost(
+            @PathVariable Long id
+    ) {
+
         postRepository.deleteById(id);
     }
 
     @PutMapping("/{id}/like")
-    public Post likePost(@PathVariable Long id) {
+    public Post likePost(
+            @PathVariable Long id
+    ) {
 
         Post post = postRepository.findById(id)
-            .orElseThrow();
+                .orElseThrow();
 
-        post.setLikes(post.getLikes() + 1);
+        post.setLikes(
+                post.getLikes() + 1
+        );
 
         return postRepository.save(post);
     }
-
-
-
 }
